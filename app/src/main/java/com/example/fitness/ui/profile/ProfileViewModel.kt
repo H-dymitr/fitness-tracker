c843e84ac41efa71721fd0af398082df06a7621c
dima <ip.web.so@gmail.com>
2023-04-04 11:16:15 +0200

    create profile page

---
+++ app/src/main/java/com/example/fitness/ui/profile/ProfileViewModel.kt
@@ -0,0 +1,33 @@
+package com.example.fitness.ui.profile
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.fitness.utils.BMICalcUtil
+
+class ProfileViewModel : ViewModel() {
+    // Declare the data fields as MutableLiveData to allow them to be observed
+    private val _name = MutableLiveData<String>()
+    private val _age = MutableLiveData<Int>()
+    private val _height = MutableLiveData<Int>()
+    private val _weight = MutableLiveData<Int>()
+    private val _BMI = MutableLiveData<String>()
+
+    // Expose the data fields as LiveData to allow read-only access from the UI
+    val name: LiveData<String> = _name
+    val age: LiveData<Int> = _age
+    val height: LiveData<Int> = _height
+    val weight: LiveData<Int> = _weight
+    val BMI: LiveData<String> = _BMI
+
+    init {
+        // TODO: Load data from the database
+        _name.value = "Jan Kowalski"
+        _age.value = 20
+        _height.value = 180
+        _weight.value = 80
+        // calculateBMI
+        val bmi = BMICalcUtil.instance.calculateBMIMetric(_height.value!!.toDouble(), _weight.value!!.toDouble())
+        _BMI.value = BMICalcUtil.instance.classifyBMI(bmi) + " (${bmi})"
+    }
+}
---
+++ app/src/main/java/com/example/fitness/utils/BMI.kt
@@ -0,0 +1,31 @@
+package com.example.fitness.utils
+
+import java.math.BigDecimal
+
+class BMICalcUtil {
+    fun calculateBMIMetric(heightCm: Double, weightKg: Double): Double {
+        // round to 2 decimal
+        return (weightKg / ((heightCm / CENTIMETERS_IN_METER) * (heightCm / CENTIMETERS_IN_METER))).toBigDecimal().setScale(2, BigDecimal.ROUND_HALF_UP).toDouble()
+    }
+
+    fun classifyBMI(bmi: Double): String {
+        return if (bmi < 18.5) {
+            BMI_CATEGORY_UNDERWEIGHT
+        } else if (bmi >= 18.5 && bmi < 25) {
+            BMI_CATEGORY_HEALTHY
+        } else if (bmi >= 25 && bmi < 30) {
+            BMI_CATEGORY_OVERWEIGHT
+        } else {
+            BMI_CATEGORY_OBESE
+        }
+    }
+
+    companion object {
+        val instance = BMICalcUtil()
+        private const val CENTIMETERS_IN_METER = 100
+        const val BMI_CATEGORY_UNDERWEIGHT = "Niedowaga"
+        const val BMI_CATEGORY_HEALTHY = "Zdrowa waga"
+        const val BMI_CATEGORY_OVERWEIGHT = "Nadwaga"
+        const val BMI_CATEGORY_OBESE = "Otyłość"
+    }
+}
