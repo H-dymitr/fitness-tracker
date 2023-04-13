58287f4da07f615cb7e66ed4a5f58fe6126cbba0
deadmouze <mateusz.kierman@gmail.com>
2023-04-13 11:19:33 +0200

    dmze (Mateusz)
    intro aplikacji

---
+++ app/src/main/java/com/example/fitness/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.fitness
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+        val introLogo = findViewById<ImageView>(R.id.intro_icon)
+
+        introLogo.alpha = 0f
+        introLogo.animate().setDuration(1500).alpha(1f).withEndAction{
+            val i = Intent(this,MainActivity::class.java)
+            startActivity(i)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+            finish()
+        }
+    }
+}
