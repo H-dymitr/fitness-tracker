77f6ff0cbb3e5798e8dfceb2212c99ec44e64477
deadmouze <mateusz.kierman@gmail.com>
2023-04-14 12:43:19 +0200

    dmze (Mateusz)
    Update historii

--- app/src/main/java/com/example/fitness/ui/history/HistoryFragment.kt
+++ app/src/main/java/com/example/fitness/ui/history/HistoryFragment.kt
@@ -7,11 +7,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.example.fitness.databinding.FragmentHomeBinding
-
+import com.example.fitness.databinding.FragmentHistoryBinding
 class HistoryFragment : Fragment() {
 
-    private var _binding: FragmentHomeBinding? = null
+    private var _binding: FragmentHistoryBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -24,10 +23,21 @@ class HistoryFragment : Fragment() {
     ): View {
         ViewModelProvider(this)[HistoryViewModel::class.java]
 
-        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        _binding = FragmentHistoryBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
         // Tworzymy animację i uruchamiamy ją dla widoku tekstowego
+        ObjectAnimator.ofFloat(binding.notificationBell, "translationX", 200f, 0f)
+            .apply {
+                duration = 700
+                startDelay = 700
+                ObjectAnimator.ofFloat(binding.notificationBell, "alpha", 0f, 1f)
+                    .apply {
+                        duration = 700
+                        startDelay = 700
+                    }.start()
+            }.start()
+
         ObjectAnimator.ofFloat(binding.historyPage, "translationY", -100f, 0f)
             .apply {
                 duration = 700
@@ -54,6 +64,33 @@ class HistoryFragment : Fragment() {
             .apply {
                 duration = 700
                 startDelay = 1800
+                ObjectAnimator.ofFloat(binding.running, "alpha", 0f, 1f)
+                    .apply {
+                        duration = 700
+                        startDelay = 1800
+                    }.start()
+            }.start()
+
+        ObjectAnimator.ofFloat(binding.roundedRectangle2, "alpha", 0f, 1f)
+            .apply {
+                duration = 700
+                startDelay = 1900
+                ObjectAnimator.ofFloat(binding.cycling, "alpha", 0f, 1f)
+                    .apply {
+                        duration = 700
+                        startDelay = 1900
+                    }.start()
+            }.start()
+
+        ObjectAnimator.ofFloat(binding.roundedRectangle3, "alpha", 0f, 1f)
+            .apply {
+                duration = 700
+                startDelay = 2000
+                ObjectAnimator.ofFloat(binding.swimming, "alpha", 0f, 1f)
+                    .apply {
+                        duration = 700
+                        startDelay = 2000
+                    }.start()
             }.start()
 
         return root
