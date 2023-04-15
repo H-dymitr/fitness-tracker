0534c87b75ad8fb723192154a35e779e0dd8388a
rafaelef <rafaelefxd@gmail.com>
2023-04-15 19:16:13 +0200

    Added MAP + recording Fragment

--- app/src/main/java/com/example/fitness/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/fitness/ui/dashboard/DashboardFragment.kt
@@ -34,7 +34,7 @@ private var _binding: FragmentDashboardBinding? = null
     return root
   }
 
-override fun onDestroyView() {
+    override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
     }
