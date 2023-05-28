360bfaa69e718c8fd95506cb926ceffe75490328
rafaelef <rafaelefxd@gmail.com>
2023-05-28 16:46:34 +0200

    Added dependencies, maps still dont work

--- app/src/main/java/com/example/fitness/Recording.kt
+++ app/src/main/java/com/example/fitness/Recording.kt
@@ -1,14 +1,19 @@
 package com.example.fitness
 
+import RecordingFragment
+import android.Manifest
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
-
-class RecordingActivity : AppCompatActivity() {
+class RecordingActivity : AppCompatActivity(), OnMapReadyCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,8 +22,25 @@ class RecordingActivity : AppCompatActivity() {
         val fragmentManager: FragmentManager = supportFragmentManager
         val fragmentTransaction: FragmentTransaction = fragmentManager.beginTransaction()
 
+        val perms = arrayOf(
+            Manifest.permission.ACCESS_FINE_LOCATION,
+            Manifest.permission.ACCESS_COARSE_LOCATION,
+            Manifest.permission.INTERNET
+        )
+        requestPermissions(perms, 0)
+
         val fragment: Fragment = RecordingFragment.newInstance("wartość1", "wartość2")
         fragmentTransaction.replace(R.id.container, fragment)
         fragmentTransaction.commit()
     }
+    override fun onMapReady(googleMap: GoogleMap) {
+        val sydney = LatLng(-33.852, 151.211)
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(sydney)
+                .title("Marker in Sydney")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+    }
+
 }
